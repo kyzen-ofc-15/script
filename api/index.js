1e065d8d82c8ffc83275be3f388e30edb859af1c
kyzen-ofc-15 <kyzenzofficial@gmail.com>
2025-09-29 00:07:52 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,6 @@
 // Anda bisa mengubah isi dari RAW_CONTENT ini sesuai kebutuhan.
 const RAW_CONTENT = `loadstring(game:HttpGet("https://lunor.dev/loader"))()
 
----
-Ini adalah teks mentah yang dikirim oleh Vercel.
-Kode ini akan terlihat saat Anda mengakses URL /api.
-Versi: 1.0`;
-
 // Fungsi utama yang dijalankan Vercel
 export default (req, res) => {
   // PENTING: Mengatur Content-Type menjadi 'text/plain' agar browser menampilkannya sebagai teks murni
