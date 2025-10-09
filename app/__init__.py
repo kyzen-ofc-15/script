27117cd08e56370cb4e28c79c5377237dbe0d24d
kyzen-ofc-15 <kyzenzofficial@gmail.com>
2025-10-09 23:27:43 +0700

    Add app/__init__.py

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config')
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.views import api
+    app.register_blueprint(api)
+
+    return app
